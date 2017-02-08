c86ad59624958341e6a2389d2d8f6ec61108318d
Timo Mämecke <timo@maemecke.com>
2017-02-08 15:51:59 +0100

    Yup. Documentation.

--- app/components/ButtonList.js
+++ app/components/ButtonList.js
@@ -1,11 +1,15 @@
-
+/**
+ * Lists Buttons in a row
+ */
 
 import React from 'react'
 import { View } from 'react-native'
 
 const ButtonList = ({ children }) => {
   return (
-    <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>{children}</View>
+    <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+      {children}
+    </View>
   )
 }
 
