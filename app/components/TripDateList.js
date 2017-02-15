b3f1e047096dba5a0c57ed8454565e13efc3c1a7
Timo Mämecke <timo@maemecke.com>
2017-02-15 15:07:26 +0100

    Flow: TripDateList

--- app/components/TripDateList.js
+++ app/components/TripDateList.js
@@ -1,5 +1,34 @@
+/**
+ * Display Components side-by-side with a line between it
+ * @flow
+ */
+
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
+
+
+type Props = {
+  children: Array<ReactElement<*>>
+}
+
+const TripDateList = ({ children }: Props) => {
+  return (
+    <View style={styles.row}>
+      {children.map((child, i) => {
+        // Wrap every child in a view with its own styles.
+        // Every view except the first has a seperator style
+        const style = StyleSheet.flatten([
+          styles.item,
+          i != 0 && styles.itemSeparator
+        ])
+        return <View key={i} style={style}>{child}</View>
+      })}
+    </View>
+  )
+}
+
+export default TripDateList
+
 
 const styles = StyleSheet.create({
   row: {
@@ -18,19 +47,3 @@ const styles = StyleSheet.create({
     borderLeftColor: '#E0E0E0'
   }
 })
-
-const TripDateList = ({ children }) => {
-  return (
-    <View style={styles.row}>
-      {children.map((child, i) => {
-        const styleList = [
-          styles.item,
-          i != 0 && styles.itemSeparator
-        ]
-        return <View key={i} style={StyleSheet.flatten(styleList)}>{child}</View>
-      })}
-    </View>
-  )
-}
-
-export default TripDateList
