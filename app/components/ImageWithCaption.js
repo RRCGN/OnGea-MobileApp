e249950e750beed027c508186b94178c3f015562
Timo Mämecke <timo@maemecke.com>
2017-02-15 14:48:16 +0100

    Flow: ImageWithCaption

--- app/components/ImageWithCaption.js
+++ app/components/ImageWithCaption.js
@@ -1,10 +1,62 @@
 /**
  * Aspect Ratio Image with an overlayed caption
+ * @flow
  */
 
 import React, { Component } from 'react'
 import { View, Image, StyleSheet, Text } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
+import type { ImageSource } from 'react-native'
+
+
+export type Props = {
+  title: string,
+  subtitle?: string,
+  source: ImageSource
+}
+
+
+export default class ImageWithCaption extends Component<any, Props, any> {
+  props: Props;
+
+  state: {
+    height: number,
+    width: number
+  };
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      height: 0,
+      width: 0
+    }
+  }
+
+  handleOnLayout = (event: any): void => {
+    const { width } = event.nativeEvent.layout
+    this.setState({ height: width * (9/16), width })
+  }
+
+  render() {
+    const { height, width } = this.state
+    const { title, subtitle, source } = this.props
+
+    return (
+      <View onLayout={this.handleOnLayout} style={{ position: 'relative' }}>
+        <Image
+          resizeMode="cover"
+          style={{ height, width, ...StyleSheet.flatten(styles.image) }}
+          source={source}
+        />
+        <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.8)']} style={styles.overlay}>
+          <Text style={styles.text}>{title}</Text>
+          {subtitle && <Text style={styles.subtext}>{subtitle}</Text>}
+        </LinearGradient>
+      </View>
+    )
+  }
+}
+
 
 const styles = StyleSheet.create({
   overlay: {
@@ -32,42 +84,3 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 2
   }
 })
-
-export default class ImageWithCaption extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { height: 0, width: 0 }
-  }
-
-  handleOnLayout = (event) => {
-    const { width } = event.nativeEvent.layout
-    this.setState({ height: width * (9/16), width })
-  }
-
-  render() {
-    const { height, width } = this.state
-    const { title, subtitle, source } = this.props
-
-    return (
-      <View onLayout={this.handleOnLayout} style={{ position: 'relative' }}>
-        <Image
-          resizeMode="cover"
-          style={{ height, width, ...StyleSheet.flatten(styles.image) }}
-          source={source}
-        />
-        <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.8)']} style={styles.overlay}>
-          <Text style={styles.text}>{title}</Text>
-          {subtitle && (
-            <Text style={styles.subtext}>{subtitle}</Text>
-          )}
-        </LinearGradient>
-      </View>
-    )
-  }
-}
-
-ImageWithCaption.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  subtitle: React.PropTypes.string,
-  source: Image.propTypes.source.isRequired
-}
