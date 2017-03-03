61b4a60ab8d01c1659b6079b76bb241cb2ce8481
Timo Mämecke <timo@maemecke.com>
2017-03-03 13:40:56 +0100

    LoginView with Flow Type, somehow

--- app/views/LoginView.js
+++ app/views/LoginView.js
@@ -1,53 +1,63 @@
 /**
- *
+ * View for Login
  * @flow
  */
 
 import React, { Component } from 'react'
 import {
   View,
-  Button,
-  TextInput
+  StyleSheet
 } from 'react-native'
+import FlatButton from '../components/FlatButton'
 import TextField from 'react-native-md-textinput'
 
 
-type Props = {
+export type LoginViewProps = {
   success: ?boolean,
   onSubmit: ({ username: string, password: string }) => void
 }
 
+type LoginViewState = {
+  username: string,
+  password: string
+}
+
 export default class LoginView extends Component {
+  props: LoginViewProps
+  state: LoginViewState
 
-  state: { username: string, password: string }
+  static defaultProps = {
+    onSubmit: () => { }
+  }
 
-  constructor(props: Props) {
+  constructor(props: LoginViewProps) {
     super(props)
-
-    this.state = {
-      username: '',
-      password: ''
-    }
+    this.state = { username: '', password: '' }
   }
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <TextField
+          style={styles.formElement}
           label="Username"
           value={this.state.username}
           onChangeText={(username) => this.setState({ username })}
         />
         <TextField
+          style={styles.formElement}
           label="Password"
           secureTextEntry={true}
           value={this.state.password}
           onChangeText={(password) => this.setState({ password })}
         />
-        <Button
-          title="Login"
-          onPress={this._handleLoginPress}
-        />
+        <View style={styles.button}>
+          <FlatButton
+            label="Login"
+            styles={styles.formElement}
+            onPress={this._handleLoginPress}
+          />
+        </View>
       </View>
     )
   }
@@ -57,3 +67,13 @@ export default class LoginView extends Component {
     this.props.onSubmit({ username, password })
   }
 }
+
+
+const styles = StyleSheet.create({
+  formElement: {
+    width: 200
+  },
+  button: {
+    marginTop: 20
+  }
+})
