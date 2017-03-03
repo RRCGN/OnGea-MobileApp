f2c339db31491cfa21273935961c301e509d0087
Timo Mämecke <timo@maemecke.com>
2017-03-03 10:17:33 +0100

    Mock for ApiService

---
+++ app/services/__mocks__/ApiService.js
@@ -0,0 +1,9 @@
+export default class ApiService {
+  static async auth(username, password) {
+    if (username === 'foo' && password === 'bar') {
+      return Promise.resolve({ ok: true, token: 'foobar' })
+    } else {
+      return Promise.resolve({ ok: false })
+    }
+  }
+}
