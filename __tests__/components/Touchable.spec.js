2b886d860301e62587e7506b1080d5c5d1eccab3
Timo Mämecke <timo@maemecke.com>
2017-03-10 16:23:44 +0100

    Specs for Touchable (only ios because of facebook/jest#1370)

---
+++ __tests__/components/Touchable.spec.js
@@ -0,0 +1,25 @@
+import { View } from 'react-native'
+import React from 'react'
+import Touchable from '../../app/components/Touchable'
+
+import renderer from 'react-test-renderer'
+
+describe('<Touchable />', () => {
+
+  describe('on ios', () => {
+    test('renders correctly with default props', () => {
+      const tree = renderer.create(
+        <Touchable><View /></Touchable>
+      ).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+
+    test('renders correctly with onPress', () => {
+      const tree = renderer.create(
+        <Touchable onPress={() => {}}><View /></Touchable>
+      ).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+
+})
