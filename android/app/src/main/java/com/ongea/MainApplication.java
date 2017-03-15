275fd892edee9be0853b3ee8ca139b6adeb87507
Timo Mämecke <timo@maemecke.com>
2017-03-15 10:17:51 +0100

    Install snackbar component

--- android/app/src/main/java/com/ongea/MainApplication.java
+++ android/app/src/main/java/com/ongea/MainApplication.java
@@ -3,6 +3,7 @@ package com.ongea;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.mehcode.reactnative.splashscreen.SplashScreenPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SnackbarPackage(),
             new SplashScreenPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
