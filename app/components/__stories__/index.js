2b42985d7083898afeb5901aecdd2b313dd91355
Timo Mämecke <timo@maemecke.com>
2017-03-16 15:28:18 +0100

    ButtonList story

--- app/components/__stories__/index.js
+++ app/components/__stories__/index.js
@@ -1,9 +1,11 @@
 import Hint from './Hint.stories'
 import OGTextButton from './OGTextButton.stories'
 import OGIconButton from './OGIconButton.stories'
+import ButtonList from './ButtonList.stories'
 
 export default {
   Hint,
   OGTextButton,
-  OGIconButton
+  OGIconButton,
+  ButtonList
 }
