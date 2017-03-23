20aebd311857a7ae0c2eeb5565fdf8d863316c2f
Timo Mämecke <timo@maemecke.com>
2017-03-23 15:25:18 +0100

    More Data Types in Travel

--- app/components/OGTravelSection.js
+++ app/components/OGTravelSection.js
@@ -5,13 +5,19 @@
 
 import React from 'react'
 import { View, Text } from 'react-native'
+import moment from 'moment'
 import OGRecentData from '../containers/OGRecentData'
 import Section from './Section'
+import OGTravelDate from './OGTravelDate'
+import OGTravelDateList from './OGTravelDateList'
+import OGTextButton from './OGTextButton'
+import ButtonList from './ButtonList'
 
 
 const OGTravelSection = ({ recentIndex, data }) => {
   const {
     type,
+    number,
     dateFrom,
     dateTo,
     origin: { locationName: from },
@@ -19,12 +25,34 @@ const OGTravelSection = ({ recentIndex, data }) => {
   } = data[recentIndex]
 
   const typeLocalization = {
-    PLANE: "Flug"
+    FLIGHT: 'Nächster Flug',
+    TRAIN: 'Nächster Zug',
+    OTHER: 'Nächste Reise'
+  }
+
+  const icons = {
+    FLIGHT: [ 'airplane-takeoff', 'airplane-landing' ],
+    TRAIN: [ 'train', 'train' ],
+    OTHER: [ 'home', 'flag-triangle' ]
   }
 
   return (
-    <Section title={typeLocalization[type]}>
-      
+    <Section title={typeLocalization[type] + (!!number ? `: ${number}` : '')}>
+      <OGTravelDateList>
+        <OGTravelDate
+          time={moment(dateFrom).format('DD.MM.YYYY, HH:MM [Uhr]')}
+          location={from}
+          icon={icons[type][0]}
+        />
+        <OGTravelDate
+          time={moment(dateTo).format('DD.MM.YYYY, HH:MM [Uhr]')}
+          location={to}
+          icon={icons[type][1]}
+        />
+      </OGTravelDateList>
+      <ButtonList>
+        <OGTextButton label="Mehr" />
+      </ButtonList>
     </Section>
   )
 }
