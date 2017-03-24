949f6b6c8e3101a250d6bd2e364ea485af5c1b06
Timo Mämecke <timo@maemecke.com>
2017-03-24 09:51:09 +0100

    Changed Storybook order

--- app/components/__stories__/index.js
+++ app/components/__stories__/index.js
@@ -1,15 +1,15 @@
-import Hint from './Hint.stories'
 import Button from './Button.stories'
-import OGDate from './OGDate.stories'
 import Card from './Card.stories'
+import Hint from './Hint.stories'
+import OGDate from './OGDate.stories'
 import Section from './Section.stories'
 import OGTravelDate from './OGTravelDate.stories'
 
 export default {
   Button,
+  Card,
   Hint,
   OGDate,
-  Card,
   Section,
   OGTravelDate
 }
