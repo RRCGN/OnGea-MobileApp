c917f4b27867dba29e676bfa66c1221a2f96c6cd
Timo Mämecke <timo@maemecke.com>
2017-03-29 16:03:05 +0200

    Added react-native-maps

--- android/app/src/main/java/com/ongea/MainApplication.java
+++ android/app/src/main/java/com/ongea/MainApplication.java
@@ -3,6 +3,8 @@ package com.ongea;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.mehcode.reactnative.splashscreen.SplashScreenPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -28,6 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
+            new RNFetchBlobPackage(),
             new SnackbarPackage(),
             new SplashScreenPackage(),
             new LinearGradientPackage(),
