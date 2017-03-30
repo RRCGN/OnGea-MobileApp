5d3ce335eb6e993cb1583625855ba932e604e84b
Timo Mämecke <timo@maemecke.com>
2017-03-30 11:56:52 +0200

    List Remote Files

--- app/subviews/SectionDownloads.js
+++ app/subviews/SectionDownloads.js
@@ -6,11 +6,25 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import Section from '../components/Section'
+import ListManager from '../components/ListManager'
+import ListItemFancy from '../components/ListItemFancy'
+import filesize from 'filesize'
 
 
 const SectionDownloads = ({ data }) => {
   return (
     <Section title="Dateien">
+      <ListManager
+        items={data}
+        renderItem={(item, i) => (
+          <ListItemFancy
+            key={i}
+            primary={item.name}
+            secondary={filesize(item.size)}
+            icon="download"
+          />
+        )}
+      />
     </Section>
   )
 }
