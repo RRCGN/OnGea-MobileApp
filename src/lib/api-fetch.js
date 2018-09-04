235551bacaa5f929f1007148df4b4081453dbdba
Timo Mämecke <timo@maemecke.com>
2018-09-04 14:35:12 +0200

    Add timeout to fetch

--- src/lib/api-fetch.js
+++ src/lib/api-fetch.js
@@ -1,11 +1,22 @@
 import Cookie from 'react-native-cookie'
 
+const REQUEST_TIMEOUT = 15 * 1000 // 15sec
+
+function timeout(ms, promise) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      reject(new Error("timeout"))
+    }, ms)
+    promise.then(resolve, reject)
+  })
+}
+
 export default function apiFetch(instanceUrl, { token } = {}) {
   return (path, { body, method = 'GET' } = {}) => {
     // credentials: 'omit' somehow won't work. So let's clear any cookies.
     return Cookie.clear(instanceUrl)
       .then(() => {
-        return fetch(instanceUrl + path, {
+        return timeout(REQUEST_TIMEOUT, fetch(instanceUrl + path, {
           method,
           headers: {
             Accept: 'application/json',
@@ -15,7 +26,7 @@ export default function apiFetch(instanceUrl, { token } = {}) {
           cache: 'no-cache',
           body: JSON.stringify(body),
           credentials: 'omit'
-        })
+        }))
       })
       .then(response => {
         if (!response.ok) throw new Error('ApiError')
