3922bde93859bb9fd4a11f17568e1cd07d93a296
Timo Mämecke <timo@maemecke.com>
2018-10-26 01:22:25 +0200

    Fix Android build

--- android/app/src/main/java/eu/rootsnroutes/ongea/MainActivity.java
+++ android/app/src/main/java/eu/rootsnroutes/ongea/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ongea;
+package eu.rootsnroutes.ongea;
 
 import android.content.Intent;
 import android.os.Bundle;
--- android/app/src/main/java/eu/rootsnroutes/ongea/MainApplication.java
+++ android/app/src/main/java/eu/rootsnroutes/ongea/MainApplication.java
@@ -1,4 +1,4 @@
-package com.ongea;
+package eu.rootsnroutes.ongea;
 
 import android.app.Application;
 
@@ -61,6 +61,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    BackgroundTaskPackage.useContext(this);
   }
 }
