f0d50c84de150732281b2bbda818d29003f8ebd9
Timo Mämecke <timo@maemecke.com>
2018-11-20 19:40:11 +0100

    Add lt locale

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,31 @@
 import { setupI18n } from '@lingui/core'
 import RNLanguages from 'react-native-languages'
 
-import enMessages from '../locale/en/messages.js'
+import caMessages from '../locale/ca/messages.js'
 import deMessages from '../locale/de/messages.js'
+import elMessages from '../locale/el/messages.js'
+import enMessages from '../locale/en/messages.js'
+import esMessages from '../locale/es/messages.js'
+import frMessages from '../locale/fr/messages.js'
+import huMessages from '../locale/hu/messages.js'
+import itMessages from '../locale/it/messages.js'
+import ltMessages from '../locale/lt/messages.js'
+import nlMessages from '../locale/nl/messages.js'
+import roMessages from '../locale/ro/messages.js'
 
 export const i18n = setupI18n({
   language: RNLanguages.language.split('-')[0],
   catalogs: {
     en: enMessages,
-    de: deMessages
+    ca: caMessages,
+    de: deMessages,
+    el: elMessages,
+    es: esMessages,
+    fr: frMessages,
+    hu: huMessages,
+    it: itMessages,
+    lt: ltMessages,
+    nl: nlMessages,
+    ro: roMessages
   }
 })
